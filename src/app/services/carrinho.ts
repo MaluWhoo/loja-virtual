1422fe938799d36bb7f67f5930b7b87b872e3694
Nayara Privado Francelino <SEU_EMAIL_DO_GITHUB>
2026-09-19 20:02:09 -0300

    feat: adiciona persistencia do carrinho com localStorage

--- src/app/services/carrinho.ts
+++ src/app/services/carrinho.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -6,9 +7,15 @@ import { Injectable } from '@angular/core';
 export class CarrinhoService {
 
   private produtos: any[] = [];
+  private readonly chaveCarrinho = 'carrinho';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.carregarCarrinho();
+  }
 
   adicionarProduto(produto: any) {
     this.produtos.push(produto);
+    this.salvarCarrinho();
   }
 
   listarProdutos() {
@@ -17,9 +24,30 @@ export class CarrinhoService {
 
   removerProduto(index: number) {
     this.produtos.splice(index, 1);
+    this.salvarCarrinho();
   }
 
   limparCarrinho() {
     this.produtos = [];
+    this.salvarCarrinho();
+  }
+
+  private salvarCarrinho() {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem(
+        this.chaveCarrinho,
+        JSON.stringify(this.produtos)
+      );
+    }
+  }
+
+  private carregarCarrinho() {
+    if (isPlatformBrowser(this.platformId)) {
+      const carrinhoSalvo = localStorage.getItem(this.chaveCarrinho);
+
+      if (carrinhoSalvo) {
+        this.produtos = JSON.parse(carrinhoSalvo);
+      }
+    }
   }
 }
